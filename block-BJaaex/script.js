7a1aab1d682a576ffb4056ea02bbfe7f00ad4593
Himanshu <harryrajpoot2003@gmail.com>
2023-01-07 00:25:07 +0530

    completed

--- block-BJaaex/script.js
+++ block-BJaaex/script.js
@@ -1,120 +1,179 @@
 // Card data
 const cardsArray = [
-    {
-      name: 'shell',
-      img: 'assets/blueshell.png',
-    },
-    {
-      name: 'star',
-      img: 'assets/star.png',
-    },
-    {
-      name: 'bobomb',
-      img: 'assets/bobomb.png',
-    },
-    {
-      name: 'mario',
-      img: 'assets/mario.png',
-    },
-    {
-      name: 'luigi',
-      img: 'assets/luigi.png',
-    },
-    {
-      name: 'peach',
-      img: 'assets/peach.png',
-    },
-    {
-      name: '1up',
-      img: 'assets/1up.png',
-    },
-    {
-      name: 'mushroom',
-      img: 'assets/mushroom.png',
-    },
-    {
-      name: 'thwomp',
-      img: 'assets/thwomp.png',
-    },
-    {
-      name: 'bulletbill',
-      img: 'assets/bulletbill.png',
-    },
-    {
-      name: 'coin',
-      img: 'assets/coin.png',
-    },
-    {
-      name: 'goomba',
-      img: 'assets/goomba.png',
-    },
-  ]
+  {
+    name: 'shell',
+    img: 'assets/blueshell.png',
+  },
+  {
+    name: 'star',
+    img: 'assets/star.png',
+  },
+  {
+    name: 'bobomb',
+    img: 'assets/bobomb.png',
+  },
+  {
+    name: 'mario',
+    img: 'assets/mario.png',
+  },
+  {
+    name: 'luigi',
+    img: 'assets/luigi.png',
+  },
+  {
+    name: 'peach',
+    img: 'assets/peach.png',
+  },
+  {
+    name: '1up',
+    img: 'assets/1up.png',
+  },
+  {
+    name: 'mushroom',
+    img: 'assets/mushroom.png',
+  },
+  {
+    name: 'thwomp',
+    img: 'assets/thwomp.png',
+  },
+  {
+    name: 'bulletbill',
+    img: 'assets/bulletbill.png',
+  },
+  {
+    name: 'coin',
+    img: 'assets/coin.png',
+  },
+  {
+    name: 'goomba',
+    img: 'assets/goomba.png',
+  },
+];
 
-const game = document.getElementById('game')
-const grid = document.createElement('section')
-grid.setAttribute('class', 'grid')
-game.append(grid)
+let timer = document.querySelector(".timer");
+let moves = document.querySelector(".moves");
+let move = document.querySelector(".move");
+let seconds = document.querySelector(".second");
+let minutes = document.querySelector(".minute");
+let reset = document.querySelector(".reset");
+let matchWin = document.querySelector(".win-Match");
+let countMoveTime = document.querySelector(".count-Move-time");
+let start = true;
+let allMatch = 0;
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let minute = 0;
+let second = 0;
+let delay = 1200;
+let delay2 = 1250;
+
+const game = document.getElementById('game');
+const grid = document.createElement('section');
+grid.setAttribute('class', 'grid');
+game.append(grid);
 // Duplicate array to create a match for each card
-let gameGrid = cardsArray.concat(cardsArray)
+let gameGrid = cardsArray.concat(cardsArray);
 // Randomize game grid on each load
-gameGrid.sort(() => 0.5 - Math.random())
+gameGrid.sort(() => 0.5 - Math.random());
 
-let firstGuess = ''
-let secondGuess = ''
-let count = 0
-let previousTarget = null
-let delay = 1200
+function timeCoundown() {
+  startTime = setInterval(function () {
+    if (second === 60) {
+      minute = minute + 1
+      minutes.innerText = minute
+      second = 0
+      seconds.innerText = second
+    } else if (second < 60) {
+      second = second + 1
+      seconds.innerText = second
+    }
+  }, 1000)
+};
 
 // For each item in the gameGrid array...
-gameGrid.forEach((elm)=>{
-  let card = document.createElement("div")
+gameGrid.forEach((elm) => {
+  let card = document.createElement("div");
   card.classList.add("card")
   card.dataset.name = elm.name
-  let front = document.createElement("div")
+  let front = document.createElement("div");
   front.classList.add('front')
-  let back = document.createElement("div")
+  let back = document.createElement("div");
   back.classList.add("back")
-  back.style.backgroundImage=`url(${elm.img})`
-  card.append(front,back)
+  back.style.backgroundImage = `url(${elm.img})`
+  card.append(front, back)
   grid.append(card)
-})
+});
 
-
-grid.addEventListener("click" ,(event)=>{
+// eventlistener-to-grid
+grid.addEventListener("click", (event) => {
   let clicked = event.target
-  if(clicked.nodeName==="SECTION"|| clicked === previousTarget || clicked.parentNode.classList.contains('selected')){return}
-  if(count<2){
+  if (start) {
+    start = false
+    timeCoundown()
+  }
+  if (clicked.nodeName === "SECTION" || clicked.parentNode.classList.contains('selected') || clicked.parentNode.classList.contains('match')) { return }
+  if (count < 2) {
     count++
-    if(count===1){
-      firstGuess=clicked.parentNode.dataset.name
+    if (count === 1) {
+      firstGuess = clicked.parentNode.dataset.name
+
       clicked.parentNode.classList.add("selected")
-    }else{
+    } else {
       secondGuess = clicked.parentNode.dataset.name
       clicked.parentNode.classList.add("selected")
+      move.innerText = ++move.innerText
     }
-    if(firstGuess !=="" && secondGuess !==""){
-      if(firstGuess===secondGuess){
-       setTimeout(match,delay)
-       setTimeout(resetguesses,delay)
+    if (firstGuess !== "" && secondGuess !== "") {
+      if (firstGuess === secondGuess) {
+        allMatch = allMatch + 1
+        setTimeout(match, delay)
+        setTimeout(win, delay2)
+        setTimeout(resetguesses, delay)
+      } else {
+        setTimeout(resetguesses, delay)
+      };
+    };
+  };
 
-      }else{
-        setTimeout(resetguesses,delay)
-      }
-    }
-    previousTarget = clicked
-  }
-  
-})
+});
 
-let match =()=>{
+// matchCard
+let match = () => {
   let selected = document.querySelectorAll(".selected")
-  selected.forEach((elm)=> elm.classList.add("match"))
-}
+  selected.forEach((elm) => {
+    elm.classList.add("match")
+    elm.classList.add("selectedTwo")
+  });
 
-let resetguesses =()=>{
-   firstGuess = ''
-   secondGuess = ''
-   count = 0
-   let selected = document.querySelectorAll(".selected")
-   selected.forEach((elm)=>elm.classList.remove("selected"))
-}
+};
+
+// resetguessses
+let resetguesses = () => {
+  firstGuess = ''
+  secondGuess = ''
+  count = 0
+  let selected = document.querySelectorAll(".selected")
+  selected.forEach((elm) => elm.classList.remove("selected"))
+};
+
+// resetBtn
+reset.addEventListener("click", () => {
+  window.location.reload()
+
+});
+
+// match-Win
+function win() {
+  if (allMatch === 12) {
+    game.innerHTML = ""
+    matchWin.innerText = "Match-Win"
+    matchWin.classList.add('win-Match')
+    countMoveTime.innerText = `${moves.innerText} and ${timer.innerText} Time`
+    countMoveTime.classList.add("count-Move-time")
+    clearInterval(startTime)
+  };
+};
+matchWin.classList.remove("win-Match");
+countMoveTime.classList.remove("count-Move-time");
+
